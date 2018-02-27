46b576419c0a672ea720d8fdd402da784c8597b3
Claudia Carrillo <ccarrillo@nearsoft.com>
2018-02-27 16:34:27 -0600

    Refactor User totalTasksForToday and remove whitespace

--- MyTasks/Models/User.swift
+++ MyTasks/Models/User.swift
@@ -10,53 +10,34 @@ import Foundation
 import RealmSwift
 
 @objcMembers class User: Object {
-  
   // MARK: - Properties
   dynamic var id = ""
   dynamic var firstName = ""
   dynamic var lastName = ""
   dynamic var timestamp = Date().timeIntervalSinceReferenceDate
   dynamic var isLoggedIn = false
+  
   let tasklists = LinkingObjects(fromType: TaskList.self, property: "user")
+  var imageURL: String { return "https://graph.facebook.com/\(id)/picture?type=large" }
   
-  var imageURL: String {
-    return "https://graph.facebook.com/\(id)/picture?type=large"
-  }
-  
-  var totalTasksForToday: Int {
-    var total = 0
-    for tasklist in tasklists {
-      total += tasklist.pendingTasksToday.count
-    }
-    return total
-  }
+  var totalTasksForToday: Int { return tasklists.reduce(0) { total, tasklist in tasklist.pendingTasksToday.count } }
   
   // MARK: - Init
   convenience init(with facebookData: [String : Any]?) {
     self.init()
-    
-    if let id = facebookData?["id"] as? String {
-      self.id = id
-    }
-    
-    if let firstName = facebookData?["first_name"] as? String {
-      self.firstName = firstName
-    }
-    
-    if let lastName = facebookData?["last_name"] as? String {
-      self.lastName = lastName
-    }
+    if let id = facebookData?["id"] as? String { self.id = id }
+    if let firstName = facebookData?["first_name"] as? String { self.firstName = firstName }
+    if let lastName = facebookData?["last_name"] as? String { self.lastName = lastName }
   }
   
   // MARK: - Meta
   override class func primaryKey() -> String? {
     return "id"
   }
-  
   override static func indexedProperties() -> [String] {
     return ["isLoggedIn"]
   }
-  
+
   // MARK: - Manage user methods
   func logOut() {
     RealmService.logOut(user: self)
@@ -67,7 +48,6 @@ extension User: BasicStorageFunctions {
   func add() {
     RealmService.add(object: self, set: { isLoggedIn = true }, update: true)
   }
-  
   func hardDelete() {
     RealmService.hardDelete(object: self)
   }
